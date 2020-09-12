3cbce07a9714418976e50c6b929839be67f943b1
merceyz <merceyz@users.noreply.github.com>
2020-09-13 01:23:15 +0200

    feat(vscode-zipfs): handle virtual links in terminal

--- packages/vscode-zipfs/sources/index.ts
+++ packages/vscode-zipfs/sources/index.ts
@@ -14,7 +14,44 @@ function mount(uri: vscode.Uri) {
   }
 }
 
+type Link = {
+  startIndex: number;
+  length: number;
+  tooltip: string;
+  data: string;
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  // @ts-expect-error - Types are not up to date
+  vscode.window.registerTerminalLinkProvider({
+    // @ts-expect-error - Types are not up to date
+    provideTerminalLinks: context => {
+      const line = context.line as string;
+      const startIndex = line.indexOf(`.yarn/$$virtual`);
+      if (startIndex === -1) return [];
+
+      const linkResult: Array<Link> = [];
+
+      const matcher = /[^\s]*\.yarn\/\$\$virtual[^\s]+/g;
+      let match: RegExpExecArray | null = null;
+      while ((match = matcher.exec(line))) {
+        linkResult.push({
+          startIndex: match.index,
+          length: match[0].length,
+          data: match[0],
+          tooltip: `Open file in editor`,
+        });
+      }
+
+      return linkResult;
+    },
+    handleTerminalLink: async (link: Link) => {
+      const parsed = vscode.Uri.parse(`zip:${link.data.replace(/^\/?/, `/`)}`);
+
+      await vscode.window.showTextDocument(parsed);
+    },
+  });
+
   context.subscriptions.push(vscode.workspace.registerFileSystemProvider(`zip`, new ZipFSProvider(), {
     isCaseSensitive: true,
   }));
